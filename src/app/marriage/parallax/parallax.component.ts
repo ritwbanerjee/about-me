408176f52ad050195e87ff6a6830892114911a63
Ritwik Banerjee <ritwbanerjee@deloitte.com>
2019-01-20 16:01:39 +0530

    Fix parallax for mobile

--- src/app/marriage/parallax/parallax.component.ts
+++ src/app/marriage/parallax/parallax.component.ts
@@ -1,14 +1,28 @@
-import { Component, AfterViewInit } from '@angular/core';
+import { Component, AfterViewInit, OnInit } from '@angular/core';
 declare var jQuery: any;
 
 @Component({
     selector: 'app-marriage-parallax',
     templateUrl: './parallax.component.html',
+    styleUrls: ['./parallax.component.scss']
 })
 
-export class MarriageParallaxComponent implements AfterViewInit {
+export class MarriageParallaxComponent implements AfterViewInit, OnInit {
+
+    windowWidth: number;
+    imageUrl: string;
 
     ngAfterViewInit() {
         jQuery('.parallax').parallax();
+    }
+
+    ngOnInit() {
+        this.windowWidth = window.innerWidth;
+
+        if (this.windowWidth <= 468) {
+            this.imageUrl = '../../../assets/konnagar.JPG';
+        } else {
+            this.imageUrl = '../../../assets/stairs.jpg';
+        }
     }
 }
