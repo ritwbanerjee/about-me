87631d8a9ef2f7ee70c23ce91b847c51cb533966
Ritwik Banerjee <ritwik.banerjee@allianz.de>
2020-05-06 01:12:08 +0200

    update the prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,40 @@
 export const environment = {
-  production: true
+  production: true,
+  nav: [
+    {
+      id: 'marriage',
+      url: '/',
+      name: 'Marriage'
+    },
+    {
+      id: 'aboutme',
+      url: '/aboutme',
+      name: 'About Me'
+    },
+    {
+      id: 'login',
+      url: '/login',
+      name: 'Login'
+    },
+    {
+      id: 'register',
+      url: '/signup',
+      name: 'Register'
+    }
+  ],
+  loginOptions: [
+    {
+      id: 'emailId',
+      placeholder: 'Enter Username',
+      label: 'Username',
+      type: 'email',
+      error: 'Please enter a valid username'
+    },
+    {
+      id: 'password',
+      placeholder: 'Enter Password',
+      label: 'Password',
+      type: 'password'
+    }
+  ]
 };
